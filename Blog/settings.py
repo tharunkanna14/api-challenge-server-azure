5d1728f70da4b5c513aa97fe13374db328ad965a
Tharun Kanna <66260870+tharunkanna14@users.noreply.github.com>
2021-12-02 09:27:03 +0530

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/home/thdjangoblog/My-Django-Blog/static'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
